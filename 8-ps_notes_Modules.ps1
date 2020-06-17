f5b4eb4223ade7023bbbcb5c289e957312d42043
dave <web8341@gmail.com>
2020-06-16 20:48:50 -0400

    continue

---
+++ 8-ps_notes_Modules.ps1
@@ -0,0 +1,15 @@
+﻿# Start transaction log
+Start-Transcript
+
+# Before Powershell will not let you create your own modules
+Set-ExecutionPolicy -Scope CurrentUser Unrestricted -Force
+
+# Profiles
+$profile
+
+Get-Content $profile
+
+
+
+
+
